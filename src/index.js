3cfa342e8c2ddbfe82794ada7e72d41bbb650a98
Mauricio Santos <mauriciosantosespino1807@gmail.com>
2022-07-17 06:17:08 -0500

    Rutas agregadas

--- src/index.js
+++ src/index.js
@@ -14,29 +14,34 @@ import Hora_dia2 from './components/Hora_dia2'
 import Hora_dia3 from './components/Hora_dia3'
 import Hora_dia4 from './components/Hora_dia4'
 import Hora_dia5 from './components/Hora_dia5'
+import Calendar from './components/Calendar';
+import LoginPac from './pages/LoginPac';
+import RegistroPac from './pages/RegistroPac';
+import MisCitas from './pages/MisCitas';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
      <Routes>
-     <Route path='/home' element={<Decide/>}/>
-     <Route path='/menu' element={<Menu/>}/>
-      <Route path='/cale' element={<Calendario/>}/>
-      <Route path='/weed' element={<Pag_principal/>}/>
-      <Route path="/dia1" element={<Hora_dia1/>}/>
-      <Route path="/dia2" element={<Hora_dia2/>}/>
-      <Route path="/dia3" element={<Hora_dia3/>}/>
-      <Route path="/dia4" element={<Hora_dia4/>}/>
-      <Route path="/dia5" element={<Hora_dia5/>}/>
-      
+      <Route path='/home' element={<Decide/>}/>
+      <Route path='/menu' element={<Menu/>}/>
+        <Route path='/cale' element={<Calendario/>}/>
+        <Route path='/weed' element={<Pag_principal/>}/>
+        <Route path='/dia1' element={<Hora_dia1/>}/>
+        <Route path='/dia2' element={<Hora_dia2/>}/>
+        <Route path='/dia3' element={<Hora_dia3/>}/>
+        <Route path='/dia4' element={<Hora_dia4/>}/>
+        <Route path='/dia5' element={<Hora_dia5/>}/>
+        <Route path='/calendario' element={<Calendar/>}/>
+        <Route path='/login_paciente' element={<LoginPac/>}/>
+        <Route path='/registro_pac' element={<RegistroPac/>}/>
+        <Route path='/MisCitas' element={<MisCitas/>}/>
+        
      </Routes>
     </BrowserRouter>
     
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
