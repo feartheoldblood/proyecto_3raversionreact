29ada7bd9569de490e696886a69c67ef380e7c08
Mauricio Santos <mauriciosantosespino1807@gmail.com>
2022-07-18 05:31:32 -0500

    JS para llenar MisCitas

---
+++ src/dataCita.js
@@ -0,0 +1,25 @@
+export default [
+    {
+        especialidad:"Dermatología",
+        id: "12",
+        fecha: "2022-07-24",
+        horainicio:"17:00",
+        horafin:"17:15",
+        modalidad: "Presencial",
+        direccion: "Calle Los Pinos 123 - San Isidro",
+        medico: "Manuel Enrique Pardo Palacios"
+    },
+    {
+        especialidad:"Pediatría",
+        id: "14",
+        fecha: "2022-07-25",
+        horainicio:"16:00",
+        horafin:"16:15",
+        modalidad: "Online",
+        direccion: "Calle Los Pinos 245 - San Isidro",
+        medico: "Manuel Enrique Palacios"
+    }
+] 
+
+
+
